24dad22e7344c9e4faca5cd5b28ef17632e24291
Yudai-Shimizu-hand <y.s0922.1999@gmail.com>
2021-12-15 13:09:30 +0900

    index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>スポコチ</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        <h1>スポコチ</h1>
+    </body>
+</html>
